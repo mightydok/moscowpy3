7c5fa6d5e419e329a6eb6f399f3242c2ae39aa0b
Vitaliy Okulov <vitaliy.okulov@gmail.com>
2016-12-06 22:30:56 +0300

    Add for homework1 for while

---
+++ homework1/while.py
@@ -0,0 +1,30 @@
+name_list = ["Вася", "Маша", "Петя", "Валера", "Саша", "Даша"]
+
+answer_list = {
+    'ты кто?': 'Дед Пихто!',
+    'что делаешь?': 'Сижу в чатике, курю кальян',
+    'норм': 'Ну и отлично, у меня то же все норм'
+}
+
+def find_person(name):
+    while True:
+        item = name_list.pop()
+        if name == item:
+            return name
+            break
+
+def ask_user():
+    while True:
+        question = input('Как дела?: ').lower().strip()
+        if question in ['хорошо', 'пока']:
+            print('Пока, удачного дня тебе!')
+            break
+        else:
+            print(get_answer(question))
+
+def get_answer(question):
+    return answer_list.get(question, 'Я не понимаю тебя')
+
+if __name__ == '__main__':
+    ask_user()
+
