c97b2ceaa1512f7bb2f6382bf17c15daccd2c41b
Vitaliy Okulov <vitaliy.okulov@gmail.com>
2016-12-13 22:43:37 +0300

    Add try except for homework

--- homework2/csvwrite.py
+++ homework2/csvwrite.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 import csv
+import sys
 
 answer_list = [
     { 'key': 'ты кто?', 'value': 'Дед Пихто!' },
@@ -8,9 +9,15 @@ answer_list = [
     { 'key': 'норм', 'value': 'Ну и отлично, у меня то же все норм' }
 ]
 
-with open('export.csv', 'w', encoding='utf-8') as f:
-    fields = ['key', 'value']
-    writer = csv.DictWriter(f, fields, delimiter=';')
-    writer.writeheader()
-    for answer in answer_list:
-        writer.writerow(answer)
+try:
+    with open('export.csv', 'w', encoding='utf-8') as f:
+        fields = ['key', 'value']
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        for answer in answer_list:
+            writer.writerow(answer)
+except OSError as err:
+    print("OS error: {0}".format(err))
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
--- homework2/file.py
+++ homework2/file.py
@@ -1,10 +1,18 @@
 # encoding: utf-8
 
-with open(file='referat.txt', mode='r', encoding='utf-8') as f:
-    numwords = 0
-    for line in f:
-        line = line.replace('\n', '').split(' ')
-        if not line[0] == '':
-            numwords += len(line)
+import sys
 
-    print('Количество слов в тексте: {}'.format(numwords))
+try:
+    with open(file='referat.txt', mode='r', encoding='utf-8') as f:
+        numwords = 0
+        for line in f:
+            line = line.replace('\n', '').split(' ')
+            if not line[0] == '':
+                numwords += len(line)
+
+        print('Количество слов в тексте: {}'.format(numwords))
+except OSError as err:
+    print("OS error: {0}".format(err))
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
